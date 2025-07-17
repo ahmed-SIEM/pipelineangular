a4589d75b76173de65fe6ca030ae562762eadf92
Souid Ahmed <ahmedsouid008@gmail.com>
2025-07-17 13:13:52 +0100

    c

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,7 +11,9 @@ describe('AppComponent', () => {
 
 
 
+
   
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
