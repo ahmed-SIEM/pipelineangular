15c1a0b57015d8e721d9e49dd6d14bcf3b225908
Souid Ahmed <ahmedsouid008@gmail.com>
2025-07-17 13:18:47 +0100

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,13 +12,16 @@ describe('AppComponent', () => {
 
 
 
-  
+
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+
+  
 
   it(`should have as title 'angular-pipeline-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
